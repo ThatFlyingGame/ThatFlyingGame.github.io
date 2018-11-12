9f7d6945616864e5bf7a2e5115de0b8d546679d0
OllieFranklin <30925076+OllieFranklin@users.noreply.github.com>
2018-11-12 17:31:10 +1300

    Began menu, moved some image files

---
+++ latest/menu.js
@@ -0,0 +1,8 @@
+let menuEnvironment = [
+	new Loop("mist2",0.2,70),
+	new Loop("darkMountains2",0.4,200),
+	new Loop("darkMountains",0.5,200),
+	new Loop("mist",0.2,70),
+	new Loop("darkForest2",0.8,250),
+	new Loop("darkForest",0.8,250)
+]
